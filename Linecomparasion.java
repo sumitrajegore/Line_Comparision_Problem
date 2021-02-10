64e408cf8b98576451a163557a1ff44493ecb9b8
Sumit Rajegore <76736832+sumitrajegore@users.noreply.github.com>
2021-02-10 22:57:34 +0530

    ADDED: calculate length of line

--- Linecomparasion.java
+++ Linecomparasion.java
@@ -1,15 +1,14 @@
-public class Linecomparasion{
+public class Linecomparasion {
 
-	public static void main(String[] args){
+	public static void main(String[] args) {
 
-		System.out.println("Welcom to Line Comparison and Computation");
        int x1=0;
        int y1=0;
        int x2=4;
        int y2=3;
-       double dist;
-       dist = Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
-       System.out.println("distance between" + "("+x1+","+y1+"), " + "("+x2+","+y2+")"+"     "+dist);
-
-	}
+       double Length_of_line;
+       System.out.println("Welcom to Line Comparison and Computation");
+       Length_of_line = Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+       System.out.println("distance between two point " + "("+x1+","+y1+"), " + "("+x2+","+y2+")"+"     "+Length_of_line);
+       }
 }
