dc962ece8245e772677d075b11c8360704c67d16
Sumit Rajegore <76736832+sumitrajegore@users.noreply.github.com>
2021-02-10 23:01:13 +0530

    ADDED: check Eqality of line

--- Linecomparasion.java
+++ Linecomparasion.java
@@ -1,18 +1,15 @@
-public class Linecomparasion{
+public class Linecomparasion {
 
-	public static void main(String[] args){
+	public static void main(String[] args) {
 
-		System.out.println("Welcom to Line Comparison and Computation");
-       int x1=0;
-       int y1=0;
-       int x2=4;
-       int y2=3;
-       double dist;
-       dist = Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
-       System.out.println("distance between" + "("+x1+","+y1+"), " + "("+x2+","+y2+")"+"     "+dist);
-       System.out.println(x1==y1);
-       System.out.println(x1==y2);
-       System.out.println(x2==y1);
-       System.out.println(x2==y2);
+       int x1 = 0;
+       int y1 = 0;
+       int x2 = 4;
+       int y2 = 3;
+       System.out.println("Welcom to Line Comparison and Computation");
+       System.out.println(x1 == y1);
+       System.out.println(x1 == y2);
+       System.out.println(x2 == y1);
+       System.out.println(x2 == y2);
 	}
 }
