370afc7bdfca225e3a351690bf63e1b7f91b2b3a
Sumit Rajegore <76736832+sumitrajegore@users.noreply.github.com>
2021-02-10 23:17:28 +0530

    ADDED : checking all condition in line comparasion

--- Linecomparasion.java
+++ Linecomparasion.java
@@ -1,14 +1,14 @@
 class geometry
 {
-       static int x1=2;
-       static int y1=2;
-       static int x2=5;
-       static int y2=5;
+       static int x1 = 2;
+       static int y1 = 2;
+       static int x2 = 5;
+       static int y2 = 5;
        void line()
        {
-       double dist;
-       dist = Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
-       System.out.println("Length_of_line_is : " + "("+x1+","+y1+"), " + "("+x2+","+y2+")"+ " = " +dist);
+       double Length_of_line;
+       Length_of_line = Math.sqrt((x2-x1) * (x2-x1) + (y2-y1) * (y2-y1));
+       System.out.println("Length_of_line_is : " + "("+x1+","+y1+"), " + "("+x2+","+y2+")"+ " = " +Length_of_line);
        }
        void equal()
        {
@@ -25,14 +25,12 @@ class geometry
         System.out.println(Integer.compare(y2, x1));
        }
 }
-public class Linecomparasion
-{
-	public static void main(String[] args)
-       {
+public class Linecomparasion {
+	public static void main(String[] args) {
         System.out.println("--welcome to line comparasion problem--");
         geometry obj = new geometry();
         obj.line();
         obj.equal();
         obj.compare();
-       }
+        }
 }
