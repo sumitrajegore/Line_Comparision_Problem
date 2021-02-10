3979eea35ff440b07b55fd8b3156f44a1c51cd0c
Sumit Rajegore <76736832+sumitrajegore@users.noreply.github.com>
2021-02-10 23:12:15 +0530

    Update Linecomparasion.java

--- Linecomparasion.java
+++ Linecomparasion.java
@@ -1,21 +1,15 @@
-public class Linecomparasion{
+public class Linecomparasion {
 
-	public static void main(String[] args){
-
-		System.out.println("Welcom to Line Comparison and Computation");
-
-		 int x1=0;
-       int y1=0;
-       int x2=3;
-       int y2=3;
-       double dist;
-       dist = Math.sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2));
-       System.out.println("distancebetween" + "("+x1+","+y1+"), " + "("+x2+","+y2+")"  +  dist);
-			// as x1,y1 equals, Output will be zero
-			System.out.println(Integer.compare(x1, y1));
-			System.out.println(Integer.compare(x2, y2));
-        	System.out.println(x1 == y1);
-      	System.out.println(x2 == y1);
+	public static void main(String[] args) {
+		
+		 int x1 = 0;
+                 int y1 = 0;
+                 int x2 = 3;
+                 int y2 = 3;
+	         System.out.println(Integer.compare(x1, y1));
+	         System.out.println(Integer.compare(x2, y2));
+        	 System.out.println(Integer.compare(x1, y1));
+      	         System.out.println(Integer.compare(x2, y1));
 	}
 }
 
