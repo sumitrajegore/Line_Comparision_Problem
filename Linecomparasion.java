84277c592e3997aef3ddf6a4ebecc3ef28004c6f
Sumit Rajegore <76736832+sumitrajegore@users.noreply.github.com>
2021-02-12 00:14:45 +0530

    Update Linecomparasion.java

--- Linecomparasion.java
+++ Linecomparasion.java
@@ -2,14 +2,21 @@ public class Linecomparasion {
 
 	public static void main(String[] args) {
 
-       int x1 = 0;
-       int y1 = 0;
-       int x2 = 4;
-       int y2 = 3;
-       System.out.println("Welcom to Line Comparison and Computation");
-       System.out.println(x1 == y1);
-       System.out.println(x1 == y2);
-       System.out.println(x2 == y1);
-       System.out.println(x2 == y2);
-	}
+       int x1 = 1, x3 = 5;
+       int y1 = 2, x4 = 6;
+       int x2 = 4, y3 = 7;
+       int y2 = 3, y4 = 8;
+       int Length_of_line1;
+       int Length_of_line2;
+       System.out.println("\n Welcom to Line Comparison and Computation \n");
+       Length_of_line1 = (int) Math.sqrt( (x2-x1) * (x2-x1) + (y2-y1) * (y2-y1) );
+       Length_of_line2 = (int) Math.sqrt( (x4-x3) * (x4-x3) + (y4-y3) * (y4-y3) );
+       if (Length_of_line1 == Length_of_line2) {
+           System.out.println("Length_of_line1 and Length_of_line2 are Equal ");
+       }
+           else
+       {
+           System.out.println("Length_of_line1 and Length_of_line2 are not Equal ");
+       }
+}
 }
