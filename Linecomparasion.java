c327ca5b272403049d00ce79d7422a06828af2a2
Sumit Rajegore <76736832+sumitrajegore@users.noreply.github.com>
2021-02-11 23:02:36 +0530

    Update Linecomparasion.java

--- Linecomparasion.java
+++ Linecomparasion.java
@@ -2,13 +2,13 @@ public class Linecomparasion {
 
 	public static void main(String[] args) {
 
-       int x1 = 0;
-       int y1 = 0;
+       int x1 = 1;
+       int y1 = 2;
        int x2 = 4;
        int y2 = 3;
        double Length_of_line;
        System.out.println("Welcom to Line Comparison and Computation");
        Length_of_line = Math.sqrt( (x2-x1) * (x2-x1) + (y2-y1) * (y2-y1) );
-       System.out.println("distance between two point " + "("+x1+","+y1+"), " + "("+x2+","+y2+")"+"     "+Length_of_line);
+       System.out.println("Length_of_line =" + "("+x1+","+y1+"), " + "("+x2+","+y2+")"+"     "+Length_of_line);
        }
 }
